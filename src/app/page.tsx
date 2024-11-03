d0df7127eeb2bcfe482504f6ea4013148e71ca21
Falak sher <97186231+falak-123@users.noreply.github.com>
2024-11-04 03:20:04 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,37 +19,37 @@ import { Link as ScrollLink, Element } from "react-scroll";
 export default function Home() {
   const services = [
     {
-      icon: "../../public/images/s_6.png",
+      icon: "/Images/s_6.png",
       title: "Web Design + Development",
       description:
         "Take your business to the next level with our web design and development services",
     },
     {
-      icon: "../../public/images/s_1.png",
+      icon: "/Images/s_1.png",
       title: "Search Engine Optimization",
       description:
         "Get your website to the top of search engine results with our SEO services",
     },
     {
-      icon: "../../public/images/s_5.png",
+      icon: "/Images/s_5.png",
       title: "Content Creation",
       description:
         "Boost your brand's online presence with our social media marketing services",
     },
     {
-      icon: "../../public/images/s_3.png",
+      icon: "/images/s_3.png",
       title: "Social Media Marketing",
       description:
         "Interact with your customers and increase sales with our email marketing services",
     },
     {
-      icon: "../../public/images/s_4.png",
+      icon: "/images/s_4.png",
       title: "Email Marketing",
       description:
         "With our content creation services, we help businesses drive results",
     },
     {
-      icon: "../../public/images/s_2.png",
+      icon: "/Images/s_2.png",
       title: "Pay-Per-Click Advertising",
       description:
         "Don't waste money on ineffective advertising. Our PPC services help you reach your target audience",
